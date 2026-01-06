9dba435d072a797ea0a6c7572bd181f21c894841
MUNNAM BHAVYA <166209587+BhavyaMunnam@users.noreply.github.com>
2026-01-06 21:07:37 +0530

    Add files via upload

---
+++ datatypes.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+ int main()
+{
+	char a,b;
+	int c;
+	long int d;
+	float e;
+	double f;
+	a='A';
+	b='B';
+	c=4962;
+	d=62562;
+	e=469.232313;
+	f=123.324626352562653;
+	printf("a=%c\n",a);
+	printf("c=%d\n",c);
+	printf("b=%c\n",b);
+	printf("d=%ld\n",d);
+	printf("e=%f\n",e);
+	printf("f=%lf\n",f);
+}
