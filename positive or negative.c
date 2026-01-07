da93adac278831f60e088a075a951e0101f9380b
MUNNAM BHAVYA <166209587+BhavyaMunnam@users.noreply.github.com>
2026-01-07 22:02:44 +0530

    Add files via upload

---
+++ positive or negative.c
@@ -0,0 +1,13 @@
+
+//+ve or -ve for given number
+#include<stdio.h>
+int main()
+{
+	int n;
+	printf("enter n value");
+	scanf("%d",&n);
+	if(n>0)
+	printf("%d is +ve\n",n);
+	else
+	printf("%d is -ve\n",n);
+}
